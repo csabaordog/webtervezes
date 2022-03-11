4759824fe626057e5b7160714d5be922c21ca83b
Ördög Csaba <ordog.csaba.0@gmail.com>
2022-03-11 07:06:09 +0100

    oldal tetejere ugras

--- szkriptek/script.js
+++ szkriptek/script.js
@@ -1,7 +1,13 @@
-function bejelentkezes(){
-    alert("Bejelentkezes\nFejlesztes alatt...");
-}
-
-function regisztracio(){
-    alert("Regisztracio\nFejlesztes alatt...");
-}
+window.addEventListener("load",()=>{
+    let ugrasGomb = document.getElementById("ugras-gomb");
+    ugrasGomb.onclick = () => {
+        window.scrollTo(0,0);
+    };
+    window.onscroll = () => {
+        if(window.scrollY >= 50){
+            ugrasGomb.style.opacity = 1.0;
+        }else{
+            ugrasGomb.style.opacity = 0.0;
+        }
+    };
+})
